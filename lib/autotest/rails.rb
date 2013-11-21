f6dea48a4929511921007024e92b64d7379af839
Mike Moore <mike@blowmage.com>
2013-11-21 15:57:46 -0700

    Update rules for Rails 4
    Rails 4 moves tests from test/unit/MODEL_test.rb to test/models/MODEL_test.rb
    Update the rules to run the tests in either location.
    Allow for nested tests, so these run when app/models/widget.rb changes:
    - test/models/widget_test.rb
    - test/models/widget/associations_test.rb
    - test/models/widget/assembly_test.rb
    Update test locations for controller and helpers as well.

--- lib/autotest/rails.rb
+++ lib/autotest/rails.rb
@@ -10,51 +10,40 @@ class Autotest::Rails < Autotest
 
     clear_mappings
 
-    self.add_mapping(/^lib\/.*\.rb$/) do |filename, _|
-      impl = File.basename(filename, '.rb')
-      files_matching %r%^test/unit/#{impl}_test.rb$%
+    add_mapping %r%^lib/(.*)\.rb$% do |_, m|
+      files_matching %r%^test/(lib|unit/lib)/#{m[1]}.*_test.rb$%
       # TODO: (unit|functional|integration) maybe?
     end
 
     add_mapping %r%^test/fixtures/(.*)s.yml% do |_, m|
-      ["test/unit/#{m[1]}_test.rb",
-       "test/controllers/#{m[1]}_controller_test.rb",
-       "test/views/#{m[1]}_view_test.rb",
-       "test/functional/#{m[1]}_controller_test.rb"]
+      files_matching %r%^test/(models|controllers|views|unit|functional)/#{m[1]}.*_test.rb$%
     end
 
-    add_mapping %r%^test/(unit|integration|controllers|views|functional)/.*rb$% do |filename, _|
+    add_mapping %r%^test/.*_test\.rb$% do |filename, _|
       filename
     end
 
     add_mapping %r%^app/models/(.*)\.rb$% do |_, m|
-      "test/unit/#{m[1]}_test.rb"
-    end
-
-    add_mapping %r%^app/helpers/application_helper.rb% do
-      files_matching %r%^test/(views|functional)/.*_test\.rb$%
+      files_matching %r%^test/(models|unit)/#{m[1]}.*_test.rb$%
     end
 
     add_mapping %r%^app/helpers/(.*)_helper.rb% do |_, m|
       if m[1] == "application" then
-        files_matching %r%^test/(views|functional)/.*_test\.rb$%
+        files_matching %r%^test/(helpers|controllers|views|unit/helpers/functional)/.*_test\.rb$%
       else
-        ["test/views/#{m[1]}_view_test.rb",
-         "test/functional/#{m[1]}_controller_test.rb"]
+        files_matching %r%^test/(helpers|controllers|views|unit/helpers/functional)/#{m[1]}.*_test.rb$%
       end
     end
 
     add_mapping %r%^app/views/(.*)/% do |_, m|
-      ["test/views/#{m[1]}_view_test.rb",
-       "test/functional/#{m[1]}_controller_test.rb"]
+      files_matching %r%^test/(controllers|views|functional)/#{m[1]}.*_test.rb$%
     end
 
     add_mapping %r%^app/controllers/(.*)\.rb$% do |_, m|
       if m[1] == "application" then
         files_matching %r%^test/(controllers|views|functional)/.*_test\.rb$%
       else
-        ["test/controllers/#{m[1]}_test.rb",
-         "test/functional/#{m[1]}_test.rb"]
+        files_matching %r%^test/(controllers|views|functional)/#{m[1]}.*_test.rb$%
       end
     end
 
@@ -63,11 +52,11 @@ class Autotest::Rails < Autotest
     end
 
     add_mapping %r%^config/routes.rb$% do # FIX:
-      files_matching %r%^test/(controllers|views|functional)/.*_test\.rb$%
+      files_matching %r%^test/(integration|controllers|views|functional)/.*_test\.rb$%
     end
 
     add_mapping %r%^test/test_helper.rb|config/((boot|environment(s/test)?).rb|database.yml)% do
-      files_matching %r%^test/(unit|controllers|views|functional)/.*_test\.rb$%
+      files_matching %r%^test/(models|controllers|views|unit|functional)/#{m[1]}.*_test.rb$%
     end
   end
 
